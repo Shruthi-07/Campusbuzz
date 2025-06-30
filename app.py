e91536cd8ed89a379a8dbe84142d0cba1f0bf494
Shruthi NM <shruthiimahesh07@gmail.com>
2025-06-30 12:29:00 +0530

    message

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
 
 # Session timeout configuration
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
